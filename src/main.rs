a7eb90e2ab2277bbcdd30f68f1bd5912e8a62cf4
jcerise <jcerise06@gmail.com>
2021-09-18 17:24:48 -0600

    Initial commit - added basic map rendering

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
+
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new()
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick (&mut self, context: &mut BTerm) {
+        context.cls();
+        self.map.render(context);
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("DungeonCrawler")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
+}
