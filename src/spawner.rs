78813a65812ba6467bcb4921f3749c7467d92e30
jcerise <jcerise06@gmail.com>
2021-10-31 18:07:19 -0600

    Added in collision system, as well as some rendering improvements.

--- src/spawner.rs
+++ src/spawner.rs
@@ -12,3 +12,16 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
         )
     );
 }
+
+pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    let render = match rng.range(0, 4) {
+        0 => Render{glyph: to_cp437('E'), color: ColorPair::new(GRAY, BLACK)},
+        1 => Render{glyph: to_cp437('O'), color: ColorPair::new(GREENYELLOW, BLACK)},
+        2 => Render{glyph: to_cp437('o'), color: ColorPair::new(YELLOW, BLACK)},
+        _ => Render{glyph: to_cp437('g'), color: ColorPair::new(GREEN, BLACK)},
+    };
+
+    ecs.push(
+        (Enemy, pos, render)
+    );
+}
--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -1,16 +1,28 @@
 use crate::prelude::*;
 
 #[system]
-pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera) {
+#[read_component(Point)]
+#[read_component(Render)]
+pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
+
+    // Gather a list of all entities currently on the map. This will be used to determine
+    // whether or not a map tile should be drawn or not, as otherwise, the entity glyph gets
+    // incorrectly composed onto the map glyph beneath
+    let mut entity_points: Vec<Point> = Vec::new();
+    <(&Point, &Render)>::query()
+        .iter(ecs)
+        .for_each(|(pos, render)| {
+            entity_points.push(*pos)
+        });
 
     for y in camera.top_y ..= camera.bottom_y {
         for x in camera.left_x .. camera.right_x {
             let point = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
 
-            if map.point_in_bounds(point) {
+            if map.point_in_bounds(point) && !entity_points.contains(&point){
                 let index = map_index(x, y);
                 let glyph = match map.tiles[index] {
                     TileType::Floor => to_cp437('.'),
